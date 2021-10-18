46b331a77b2ae57c659421a23ba096d4849c6ec1
moriya1232 <moriya1232@gmail.com>
2021-10-18 16:31:38 +0300

    git care

--- app/src/main/java/com/example/myjavaapplication/FirstFragment.java
+++ app/src/main/java/com/example/myjavaapplication/FirstFragment.java
@@ -11,8 +11,6 @@ import androidx.navigation.fragment.NavHostFragment;
 
 public class FirstFragment extends Fragment {
 
-
-    
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
